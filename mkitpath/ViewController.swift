49c5a94d282e98363bf6f833f936c36b4c32e328
Ruchi Makadiya <ruchi.m@eheuristic.com>
2019-03-09 11:53:51 +0530

    Initial Commit

---
+++ mkitpath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mkitpath
+//
+//  Created by eHeuristic on 09/03/19.
+//  Copyright © 2019 eHeuristic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
